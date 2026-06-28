470cd4ebd42820e036c1d41bbbace9b2bc7815a0
Priyanshu Negi <pnegi2703@gmail.com>
2026-06-28 18:18:11 +0530

    Create shortener.model.js

---
+++ models/shortener.model.js
@@ -0,0 +1,17 @@
+import {dbClient} from "../config/db-client.js";
+import {env} from "../config/env.js";
+
+const db = dbClient.db(env.MONGODB_DATABASE_NAME);
+const shortenerCollection = db.collection('shortLinks');
+
+export const loadLinks = async() =>{
+    return shortenerCollection.find().toArray();
+};
+
+export const saveLinks = async(link)=>{
+    return shortenerCollection.insertOne(link);
+};
+
+export const getLinkByShortCode = async(shortcode) =>{
+    return await shortenerCollection.findOne({ shortCode : shortcode })
+};
